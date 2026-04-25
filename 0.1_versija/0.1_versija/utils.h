d2ffefe42af087ad238ccd6c1718190ad8b1c27b
Gytis Kaminskas <gytisskaminskas@gmail.com>
2026-04-25 15:45:19 +0300

    Update utils.h

--- 0.1_versija/0.1_versija/utils.h
+++ 0.1_versija/0.1_versija/utils.h
@@ -7,6 +7,5 @@ std::string ivesti_varda_ar_pavarde(const std::string& zinute);
 int ivesties_tikrinimas(const std::string& zinute);
 int ivesties_skaicius(const std::string& zinute);
 int skaiciavimo_metodas();
-double galutinis_vid(double x, int egz);
 
 #endif
