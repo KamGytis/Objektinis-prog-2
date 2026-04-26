018c80762e035538d6973edbaf106f374bd6f1f6
Gytis Kaminskas <gytisskaminskas@gmail.com>
2026-04-26 11:34:08 +0300

    Perkelimo testavimai

--- 0.1_versija/0.1_versija/testavimas_klases.cpp
+++ 0.1_versija/0.1_versija/testavimas_klases.cpp
@@ -38,7 +38,7 @@ static void testuoti_konstruktorius() {
 	tikrinti(a.getPavarde() == "", "Numatytasis pavarde tuscias");
 	tikrinti(a.getEgz() == 0, "Numatytasis egzaminas 0");
 	tikrinti(a.getRez() == 0.0, "Numatytasis rezultatas 0.0");
-	tikrinti(a.getPaz() == 0, "Numatytasis paz saras tuscias");
+	tikrinti(a.getPazSkaicius() == 0, "Numatytasis paz saras tuscias");
 
 	//Parametriniai konstruktoriai
 
@@ -54,10 +54,21 @@ static void testuoti_konstruktorius() {
 
 	Studentas c(b);
 
-	tikrinti(c.getVardas() == b.getVardas, "Kopijavimo vardas sutampa");
+	tikrinti(c.getVardas() == b.getVardas(), "Kopijavimo vardas sutampa");
 	tikrinti(c.getPavarde() == b.getPavarde(), "Kopijavimo pavarde sutampa");
 	tikrinti(c.getEgz() == b.getEgz(), "Kopijavimo egzaminas sutampa");
 	tikrinti(c.getPazSkaicius() == b.getPazSkaicius(), "Kopijavimo paz skaicius sutampa");
 	tikrinti(c.getPaz() == b.getPaz(), "Kopijavimo pazymiai sutampa");
 	c.setVardas("Petras");
 	tikrinti(b.getVardas() == "Jonas", "Kopijavimo gili kopija (b) nepakito");
+
+	//Perkelimo konstruktorius
+
+	Studentas d_orig("Ona", "Onaite", {5, 6}, 7);
+	Studentas d(std::move(d_orig));
+	tikrinti(d.getVardas() == "Ona", "Perkelimo vardas");
+	tikrinti(d.getPavarde() == "Onaite", "Perkelimo pavarde");
+	tikrinti(d.getEgz() == 7, "Perkelimo egzaminas");
+	tikrinti(d.getPazSkaicius() == 2, "Perkelimo paz skaicius");
+	tikrinti(d_orig.getPazSkaicius() == 0, "Perkelimo orginalas tuscias";
+	tikrinti(d_orig.getVardas() == "", "Perkelimo orginalo vardas tuscias");
