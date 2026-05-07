decc75e6fd2905f8b2747720cd29fc721a3808c8
Gytis Kaminskas <gytisskaminskas@gmail.com>
2026-05-07 18:17:13 +0300

    Update zmogus.h

--- 0.1_versija/0.1_versija/zmogus.h
+++ 0.1_versija/0.1_versija/zmogus.h
@@ -5,53 +5,111 @@
 #include <string>
 #include <iostream>
 
-// 
-// Abstrakti bazine klase Zmogus
-//
-// Negalima sukurti Zmogus tipo objektu tiesiogiai –
-// klase abstrakti del grynai virtualaus destruktoriaus.
-// Is jos galima kurti tik isvesdines klases (pvz. Studentas).
-// 
-
+/**
+ * @class Zmogus
+ * @brief Abstrakti bazine klase, atstovaujanti zmogu
+ *
+ * Negalima sukurti Zmogus tipo objektu tiesiogiai -
+ * klase abstrakti del grynai virtualaus destruktoriaus.
+ * Is jos galima kurti tik isvestines klases (pvz. Studentas).
+ *
+ * @author Studentu Valdymo Sistema
+ * @version 2.0
+ */
 class Zmogus {
 protected:
-    std::string vardas_;
-    std::string pavarde_;
+	std::string vardas_; // Vardas
+	std::string pavarde_; // Pavarde
 
 public:
     // --- Konstruktoriai (Rule of Five) ---
 
+    /**
+     * @brief Numatytasis konstruktorius
+     * Sukuria tuscia zmogu su tusciais vardu ir pavarde
+     */
     Zmogus();
+
+    /**
+     * @brief Parametrinis konstruktorius
+     * @param vardas Asmens vardas
+     * @param pavarde Asmens pavarde
+     */
     Zmogus(const std::string& vardas, const std::string& pavarde);
+
+    /**
+     * @brief Kopijavimo konstruktorius
+     * @param other Zmogus objektas, is kurio kopijuojama
+     */
     Zmogus(const Zmogus& other);
+
+    /**
+     * @brief Perkelimo konstruktorius
+     * @param other Zmogus objektas, is kurio perkeliama
+     */
+
     Zmogus(Zmogus&& other) noexcept;
 
-    // Grynas virtualus destruktorius – daro klase abstrakcia
-    // Isvestines klases privalo override'inti
+    /**
+     * @brief Virtualus destruktorius
+     *
+     * Grynas virtualus destruktorius - daro klase abstrakcia.
+     * Isvestines klases privalo ji realizuoti.
+     */
     virtual ~Zmogus() = 0;
 
     // --- Priskyrimo operatoriai (Rule of Five) ---
 
+    /**
+     * @brief Kopijavimo priskyrimo operatorius
+     * @param other Zmogus objektas, is kurio kopijuojama
+     * @return Nuoroda i si objekta
+     */
     Zmogus& operator=(const Zmogus& other);
+
+    /**
+     * @brief Perkelimo priskyrimo operatorius
+     * @param other Zmogus objektas, is kurio perkeliama
+     * @return Nuoroda i si objekta
+     */
     Zmogus& operator=(Zmogus&& other) noexcept;
 
     // --- Getteriai ---
 
+    /** @return Asmens vardas */
     const std::string& getVardas()  const { return vardas_; }
+
+    /** @return Asmens pavarde */
     const std::string& getPavarde() const { return pavarde_; }
 
     // --- Setteriai ---
 
+     /** @param v Naujas vardas */
     void setVardas(const std::string& v) { vardas_ = v; }
+
+    /** @param p Nauja pavarde */
     void setPavarde(const std::string& p) { pavarde_ = p; }
 
     // --- Grynas virtualus metodas – isvestines klases privalo realizuoti ---
 
-    // Isveda informacija apie objekta i srautua
+    /**
+     * @brief Grynas virtualus metodas spausdinimui
+     * @param os Isvesties srautas
+     *
+     * Isvestines klases privalo realizuoti si metoda.
+     * Naudojamas operator<< globaliame operatoriuje.
+     */
     virtual void print(std::ostream& os) const = 0;
 };
 
-// Globalus isvedimo operatorius naudoja virtual print()
+/**
+ * @brief Globalus isvedimo operatorius
+ * @param os Isvesties srautas
+ * @param z Nuoroda i Zmogus objekta
+ * @return Nuoroda i isvesties srauta
+ *
+ * @details Naudoja virtualu print() metoda polimorfizmui uztikrinti
+ */
 inline std::ostream& operator<<(std::ostream& os, const Zmogus& z) {
     z.print(os);
     return os;
